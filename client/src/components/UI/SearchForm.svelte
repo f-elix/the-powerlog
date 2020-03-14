ee0267315821aa46a1d9394fedcd9deea22dc74d
Felix <felix_guerin@hotmail.fr>
2020-03-14 11:52:19 -0400

    Add search form

---
+++ client/src/components/UI/SearchForm.svelte
@@ -0,0 +1,26 @@
+<script>
+  // Components
+  import Input from "@/components/UI/Input.svelte";
+
+  export let name = "";
+  export let label = "";
+</script>
+
+<style>
+  .search-form {
+    position: relative;
+    margin: 2rem auto;
+  }
+
+  .search-form .search-icon {
+    position: absolute;
+    top: 20%;
+    right: 5%;
+    z-index: 10;
+  }
+</style>
+
+<form class="search-form" on:submit|preventDefault novalidate>
+  <i class="material-icons search-icon">search</i>
+  <Input type="search" {label} {name} on:input />
+</form>
