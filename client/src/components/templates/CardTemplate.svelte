99f33c2961e3d014b74c5f83e0e7bbdbd4d0097b
Felix <felix_guerin@hotmail.fr>
2020-03-15 21:21:14 -0400

    Add card template layout

--- client/src/components/templates/CardTemplate.svelte
+++ client/src/components/templates/CardTemplate.svelte
@@ -6,20 +6,54 @@
   // Components
   import Ripple from "@/components/UI/Ripple.svelte";
 
-  export let template;
+  // export let template;
 </script>
 
 <style>
+  article {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 50rem;
+    margin: 1rem auto;
+    padding: 1rem;
+    border-radius: var(--radius-default);
+    background-color: var(--color-fg-light);
+    box-shadow: var(--shadow-default);
+  }
 
+  button {
+    width: 4rem;
+    height: 4rem;
+    border-radius: 50%;
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    line-height: 1;
+  }
+
+  button span {
+    display: none;
+  }
+
+  button.edit {
+    color: var(--color-info);
+  }
+
+  button.delete {
+    color: var(--color-error);
+  }
 </style>
 
 <article in:fly={{ x: 30 }} out:fly={{ x: 30, duration: 200 }}>
-  <!-- <h3>{template.name}</h3> -->
+  <h3>Squat Day</h3>
   <div>
-    <!-- Edit btn -->
+    <!-- View template btn -->
     <button class="edit">
-      <i class="material-icons">edit</i>
-      <span>Edit</span>
+      <i class="material-icons">visibility</i>
+      <span>View Template</span>
       <Ripple />
     </button>
     <!-- Delete btn -->
