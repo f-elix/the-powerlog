256abb5ac49128f6ccbfd2673ce90e7778112e2f
Felix <felix_guerin@hotmail.fr>
2020-03-20 20:27:25 -0400

    Add screen reader text class

--- client/src/components/exercises/CardExercise.svelte
+++ client/src/components/exercises/CardExercise.svelte
@@ -80,10 +80,6 @@
   button.save {
     color: var(--color-action);
   }
-
-  button span {
-    display: none;
-  }
 </style>
 
 <article in:fly|local={{ x: 30 }} out:fly|local={{ x: 30, duration: 200 }}>
@@ -99,13 +95,13 @@
         <!-- Save btn -->
         <button class="save" type="submit">
           <i class="material-icons">done</i>
-          <span>Save</span>
+          <span class="screen-reader-text">Save</span>
           <Ripple />
         </button>
         <!-- Discard btn -->
         <button class="delete" on:click={onDiscard}>
           <i class="material-icons">cancel</i>
-          <span>Discard</span>
+          <span class="screen-reader-text">Discard</span>
           <Ripple />
         </button>
       </div>
@@ -116,13 +112,13 @@
       <!-- Edit btn -->
       <button class="edit" on:click={onEdit}>
         <i class="material-icons">edit</i>
-        <span>Edit</span>
+        <span class="screen-reader-text">Edit</span>
         <Ripple />
       </button>
       <!-- Delete btn -->
       <button class="delete" on:click={onDelete}>
         <i class="material-icons">delete</i>
-        <span>Delete</span>
+        <span class="screen-reader-text">Delete</span>
         <Ripple />
       </button>
     </div>
