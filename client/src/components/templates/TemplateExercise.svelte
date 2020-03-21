903b1a1f2d0afdf41e8868b75e84129d849d3792
Felix <felix_guerin@hotmail.fr>
2020-03-20 21:25:42 -0400

    Add a functioning layout

---
+++ client/src/components/templates/TemplateExercise.svelte
@@ -0,0 +1,123 @@
+<script>
+  // Components
+  import Ripple from "@/components/UI/Ripple.svelte";
+  import Button from "@/components/UI/Button.svelte";
+</script>
+
+<style>
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem;
+    background-color: var(--color-fg);
+  }
+
+  li:nth-of-type(even) {
+    background-color: var(--color-fg-dark);
+  }
+
+  .content-ctn {
+    width: 65%;
+  }
+
+  .content-ctn p {
+    margin: 0;
+  }
+
+  .content-ctn .movement-name {
+    font-weight: bold;
+    font-size: var(--text-big);
+  }
+
+  .content-ctn .set {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1rem;
+  }
+
+  .content-ctn .set p {
+    width: 40%;
+  }
+
+  .add-set-btn {
+    margin: 0.5rem 0;
+    padding: 0.5rem;
+    color: var(--color-action);
+  }
+
+  button {
+    width: 4rem;
+    height: 4rem;
+    border-radius: 50%;
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    line-height: 1;
+  }
+
+  .actions-ctn {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .actions-ctn button {
+  }
+
+  .actions-ctn button.edit {
+    color: var(--color-info);
+  }
+
+  .actions-ctn button.delete {
+    color: var(--color-error);
+  }
+
+  .actions-ctn button.handle {
+    color: var(--color-primary);
+  }
+</style>
+
+<li>
+  <!-- Content -->
+  <div class="content-ctn">
+    <p class="movement-name">Movement name</p>
+    <div class="set">
+      <p>
+        <span>3</span>
+        <span>X</span>
+        <span>10</span>
+      </p>
+      <p>
+        <span>225</span>
+        <span>lbs</span>
+      </p>
+    </div>
+    <button class="add-set-btn">
+      <i class="material-icons">add_circle_outline</i>
+      <Ripple />
+    </button>
+  </div>
+  <!-- actions -->
+  <div class="actions-ctn">
+    <!-- Edit btn -->
+    <button class="edit">
+      <i class="material-icons">edit</i>
+      <span class="screen-reader-text">Edit</span>
+      <Ripple />
+    </button>
+    <!-- Delete btn -->
+    <button class="delete">
+      <i class="material-icons">delete</i>
+      <span class="screen-reader-text">Delete</span>
+      <Ripple />
+    </button>
+    <!-- Handle btn -->
+    <button class="handle">
+      <i class="material-icons">reorder</i>
+      <span class="screen-reader-text">Re-order</span>
+    </button>
+  </div>
+</li>
--- client/src/components/templates/TemplateForm.svelte
+++ client/src/components/templates/TemplateForm.svelte
@@ -2,73 +2,13 @@
   // Components
   import Input from "@/components/UI/Input.svelte";
   import Button from "@/components/UI/Button.svelte";
+  import Ripple from "@/components/UI/Ripple.svelte";
+  import TemplateExercise from "./TemplateExercise.svelte";
 </script>
 
 <style>
   .header {
     margin-bottom: 2rem;
-    padding: 1rem;
-  }
-
-  .list {
-    box-shadow: var(--default-box-shadow);
-  }
-
-  .list__header {
-    background-color: var(--color-bg-light);
-  }
-
-  .list__header th {
-    width: 22.5%;
-    padding: 0.75rem;
-  }
-
-  .exercise {
-    background-color: var(--color-fg);
-  }
-
-  .exercise:nth-of-type(even) {
-    background-color: var(--color-fg-dark);
-  }
-
-  .movement__name td {
-    font-weight: bold;
-  }
-
-  .set td {
-    width: 22.5%;
-    text-align: center;
-  }
-
-  .handle {
-    width: 10%;
-  }
-
-  .exercise-btn-ctn {
-    display: flex;
-  }
-
-  .exercise-btn-ctn button {
-    width: 4rem;
-    height: 4rem;
-    border-radius: 50%;
-    background: none;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    line-height: 1;
-  }
-
-  .exercise-btn-ctn button.edit {
-    color: var(--color-info);
-  }
-
-  .exercise-btn-ctn button.delete {
-    color: var(--color-error);
-  }
-
-  .exercise-btn-ctn button span {
-    display: none;
   }
 
   .template-btn-ctn {
@@ -81,81 +21,21 @@
   }
 </style>
 
-<form novalidate>
-  <!-- Header -->
-  <div class="header">
-    <h1>Create a Template</h1>
-    <Input label="Template Name" name="templateName" />
-  </div>
-  <!-- Exercises list -->
-  <table class="list">
-    <thead>
-      <tr class="list__header">
-        <th>Set(s)</th>
-        <th>
-          Rep(s)/
-          <br />
-          Time
-        </th>
-        <th>Load</th>
-        <th>Rest</th>
-        <th class="handle" />
-      </tr>
-    </thead>
-    <tbody class="exercise">
-      <td colspan="4">
-        <table>
-          <tr class="movement__name">
-            <td colspan="3">Movement name</td>
-            <td class="exercise-btn-ctn" colspan="2">
-              <!-- Edit btn -->
-              <button class="edit">
-                <i class="material-icons">edit</i>
-                <span>Edit</span>
-                <!-- <Ripple /> -->
-              </button>
-              <!-- Delete btn -->
-              <button class="delete">
-                <i class="material-icons">delete</i>
-                <span>Delete</span>
-                <!-- <Ripple /> -->
-              </button>
-            </td>
-          </tr>
-          <tr class="set">
-            <td>3</td>
-            <td>10 sec</td>
-            <td>315 lbs</td>
-            <td>90 sec</td>
-          </tr>
-        </table>
-      </td>
-      <td class="handle">
-        <i class="material-icons">reorder</i>
-      </td>
-    </tbody>
-    <!-- <tbody class="exercise">
-      <td colspan="5">
-        <table>
-          <tr class="movement__name">
-            <td colspan="3">Movement name</td>
-          </tr>
-          <tr class="set">
-            <td>3</td>
-            <td>1 min</td>
-            <td>80 %</td>
-            <td>3 min 30 sec</td>
-          </tr>
-        </table>
-      </td>
-      <td class="handle">
-        <i class="material-icons">reorder</i>
-      </td>
-    </tbody> -->
-
-  </table>
-  <div class="template-btn-ctn">
-    <Button color="error" variant="filled">Cancel</Button>
-    <Button color="action" variant="filled">Create</Button>
-  </div>
-</form>
+<!-- Header -->
+<div class="header">
+  <h1>Create a Template</h1>
+  <Input label="Template Name" name="templateName" />
+</div>
+<!-- Exercises list -->
+<ul class="exercise-list">
+  <TemplateExercise />
+  <TemplateExercise />
+</ul>
+<Button size="big" color="action">
+  <i class="material-icons">add_box</i>
+  Add Exercise
+</Button>
+<div class="template-btn-ctn">
+  <Button color="error" variant="filled">Cancel</Button>
+  <Button color="action" variant="filled">Create</Button>
+</div>
