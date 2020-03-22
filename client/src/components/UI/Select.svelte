80dcb65eda66cec5df76a3f57dbc421dadc5fa71
Felix <felix_guerin@hotmail.fr>
2020-03-22 12:12:30 -0400

    Change styling

--- client/src/components/UI/Select.svelte
+++ client/src/components/UI/Select.svelte
@@ -8,13 +8,13 @@
 <style>
   label {
     display: block;
-    font-size: var(--text-big);
+    font-size: var(--text-normal);
   }
 
   select {
-    padding: 0 1rem;
+    padding: 0.5rem 1rem;
     border-radius: var(--radius-default);
-    border: var(--border-default) var(--color-primary);
+    border: var(--border-thin) var(--color-primary);
     background-color: var(--color-fg-dark);
     color: var(--color-primary);
     text-transform: capitalize;
