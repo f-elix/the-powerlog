5bec120c4409bf27cb5522d7890e1e6a6b3f095c
Felix <felix_guerin@hotmail.fr>
2020-05-14 21:20:58 -0400

    Add console log

--- client/src/components/Index/Title.svelte
+++ client/src/components/Index/Title.svelte
@@ -14,6 +14,7 @@
 
     window.addEventListener("beforeinstallprompt", e => {
       e.preventDefault();
+      console.log("before install prompt fired");
       deferredPrompt = e;
       installBtn.style.removeProperty("display");
       return false;
