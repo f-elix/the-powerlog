6e0c22ede8517f10037c4abeb9bf039e14b5d085
Felix <felix_guerin@hotmail.fr>
2020-05-17 13:44:14 -0400

    Initial commit?

---
+++ client/src/fsm/exercises/exerciseMachine.js
@@ -0,0 +1,99 @@
+import { Machine, assign } from 'xstate';
+import { getData, getToken } from '@/js/utils.js';
+
+const services = {
+	getExercise: async (context, _) => {
+		const queryName = 'getExerciseById';
+		const query = {
+			query: `
+				query getExercise($exerciseId: ID!) {
+					getExerciseById(exerciseId: $exerciseId) {
+						history {
+							session {
+								_id
+								date
+							}
+							executions {
+								_id
+								sets
+								reps
+								time {
+									amount
+									unit
+								}
+								load {
+									amount
+									unit
+								}
+							}
+						}
+					}
+				}
+			`,
+			variables: {
+				exerciseId: context.exerciseId
+			}
+		};
+		try {
+			const token = getToken();
+			const data = await getData(query, queryName, token);
+			console.log(data);
+			return data;
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	}
+};
+
+const actions = {
+	updateExercise: assign({
+		exercise: (_, event) => event.data
+	}),
+	updateFetchError: assign({
+		fetchError: (_, event) => event.data
+	}),
+	clearFetchError: assign({
+		fetchError: ''
+	})
+};
+
+export const exerciseMachine = exerciseId => {
+	return Machine(
+		{
+			id: 'exercise',
+			context: {
+				exerciseId,
+				exercise: null,
+				fetchError: ''
+			},
+			initial: 'fetching',
+			states: {
+				fetching: {
+					invoke: {
+						src: 'getExercise',
+						onDone: {
+							target: 'idle',
+							actions: ['updateExercise']
+						},
+						onError: {
+							target: 'idle.error',
+							actions: ['updateFetchError']
+						}
+					}
+				},
+				idle: {
+					initial: 'normal',
+					states: {
+						normal: {},
+						error: {}
+					}
+				}
+			}
+		},
+		{
+			services,
+			actions
+		}
+	);
+};
