3e361e8e0febc53f3e3f08008c8729717aa23fc8
Felix Guerin <guerinfelix08@gmail.com>
2020-12-29 14:45:09 -0500

    Use ui object

---
+++ src/ui.ts
@@ -0,0 +1,16 @@
+export const ui: Record<string, string | Record<string, string>> = {
+	appName: 'Powerlog',
+	tagline: 'Log your training, get the results.',
+	login: 'Login',
+	signup: 'Sign Up',
+	logout: 'Logout',
+	dashboardTitle: 'Log History',
+	filterBy: 'Filter by',
+	filters: {
+		name: 'Name',
+		date: 'Date',
+		period: 'Period',
+		daysAgo: 'Days ago',
+		weeksAgo: 'Weeks ago'
+	}
+};
--- src/views/Auth.svelte
+++ src/views/Auth.svelte
@@ -3,6 +3,7 @@
 	import type { View } from '../lib/router/types';
 	import Button from '@/components/Button.svelte';
 	import Icon from '@/components/Icon.svelte';
+	import { ui } from '@/ui';
 
 	export let props: Record<string, unknown>;
 	export let children: View[];
@@ -18,11 +19,11 @@
 <section class="flex flex-col items-center justify-center h-full space-y-110">
 	<div class="flex flex-col items-center space-y-110 max-w-180 mx-auto px-50 text-center">
 		<Icon />
-		<h1 class="text-70 font-bold">Powerlog</h1>
-		<h2 class="text-50">Log your training, get the results.</h2>
+		<h1 class="text-70 font-bold">{ui.appName}</h1>
+		<h2 class="text-50">{ui.tagline}</h2>
 	</div>
 	<div class="flex flex-col space-y-50 w-full px-50">
-		<Button on:click={onLogin}>Login</Button>
-		<Button variant="outlined" on:click={onSignUp}>Sign Up</Button>
+		<Button on:click={onLogin}>{ui.login}</Button>
+		<Button variant="outlined" on:click={onSignUp}>{ui.signup}</Button>
 	</div>
 </section>
