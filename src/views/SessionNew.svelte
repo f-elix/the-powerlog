ea2662c372b4b15343841234a9e6e03df498c188
Felix Guerin <guerinfelix08@gmail.com>
2021-01-25 23:04:20 -0500

    Add section and spinner

--- src/views/SessionNew.svelte
+++ src/views/SessionNew.svelte
@@ -2,12 +2,16 @@
 	// Types
 	import type { View, ViewProps } from '../lib/router/types';
 	import type { User } from 'netlify-identity-widget';
+	// Svelte
+	import { fly } from 'svelte/transition';
+	import { expoOut } from 'svelte/easing';
 	// Stores
 	import { session } from 'src/stores/session';
 	// Ui
 	import { ui } from 'src/ui';
 	// Components
 	import SessionForm from 'coms/SessionForm.svelte';
+	import Spinner from 'coms/Spinner.svelte';
 
 	export let props: ViewProps;
 	export let children: View[];
@@ -20,4 +24,13 @@
 	$session.send({ type: 'CREATE', data: { token } });
 </script>
 
-<SessionForm {token} {exercises} title={ui.creatingSession} />
+<section class="space-y-100 pb-160" in:fly|local={{ y: -60, easing: expoOut, duration: 600 }}>
+	{#if $session.state.matches('fetching')}
+		<div class="flex items-center justify-center h-100vh">
+			<Spinner />
+		</div>
+	{/if}
+	{#if $session.state.matches('editing')}
+		<SessionForm {token} {exercises} title={ui.creatingSession} />
+	{/if}
+</section>
