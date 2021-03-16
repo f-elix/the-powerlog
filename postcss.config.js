935d27fdbbefcf40c9e16066f404a85a99196066
Felix Guerin <guerinfelix08@gmail.com>
2021-03-15 21:01:38 -0400

    Add tailwind jit plugin

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const prod = process.env.NODE_ENV === 'production';
 module.exports = {
 	plugins: [
 		require('postcss-import'),
-		require('tailwindcss'),
+		require('@tailwindcss/jit'),
 		require('autoprefixer'),
 		prod && require('cssnano')
 	].filter(Boolean)
